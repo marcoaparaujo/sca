8c2159b15953e15fa215a4cda8957cd67a7d3d78
Marco Araujo <marcoaparaujo@gmail.com>
2021-03-04 17:51:58 -0300

    bootstrap

--- sca/settings.py
+++ sca/settings.py
@@ -142,4 +142,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 LOGOUT_REDIRECT_URL = 'index'
