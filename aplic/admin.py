3d6c5fe0bbe821edfdb0d22ffc7383055039c204
Marco Araujo <marcoaparaujo@gmail.com>
2021-03-12 16:28:36 -0300

    classe relacionamento tipo 1

--- aplic/admin.py
+++ aplic/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from .models import Curso
+from .models import Curso, Professor
 
 
 @admin.register(Curso)
 class CursoAdmin(admin.ModelAdmin):
     list_display = ('nome', 'carga_horaria')
+
+
+@admin.register(Professor)
+class ProfessorAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'titulacao', 'curso')
--- aplic/models.py
+++ aplic/models.py
@@ -13,3 +13,21 @@ class Curso(models.Model):
     def __str__(self):
         return self.nome
 
+
+class Professor(models.Model):
+    OPCOES = (
+        ('Doutorado',       'Doutorado'),
+        ('Mestrado',        'Mestrado'),
+        ('Especialização',  'Especialização'),
+        ('Graduação',       'Graduação'),
+    )
+    nome = models.CharField('Nome', max_length=100)
+    titulacao = models.CharField('Titulação', blank=True, max_length=100, choices=OPCOES)
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Professor'
+        verbose_name_plural = 'Professores'
+
+    def __str__(self):
+        return self.nome
