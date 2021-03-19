011e9dbcd5faa8d9b41a24d114353c8be0bd3b36
Marco Araujo <marcoaparaujo@gmail.com>
2021-03-19 17:46:01 -0300

    teste

--- aplic/admin.py
+++ aplic/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Curso, Professor
+from .models import Curso, Professor, Aluno, Disciplina, Turma
 
 
 @admin.register(Curso)
@@ -11,3 +11,19 @@ class CursoAdmin(admin.ModelAdmin):
 @admin.register(Professor)
 class ProfessorAdmin(admin.ModelAdmin):
     list_display = ('nome', 'titulacao', 'curso')
+
+
+@admin.register(Aluno)
+class AlunoAdmin(admin.ModelAdmin):
+    list_display = ('matricula', 'nome', 'curso')
+
+
+@admin.register(Disciplina)
+class DisciplinaAdmin(admin.ModelAdmin):
+    list_display = ('curso', 'nome', 'carga_horaria')
+
+
+@admin.register(Turma)
+class TurmaAdmin(admin.ModelAdmin):
+    list_display = ('ano', 'semestre', 'turma', 'disciplina')
+
--- aplic/models.py
+++ aplic/models.py
@@ -14,20 +14,75 @@ class Curso(models.Model):
         return self.nome
 
 
-class Professor(models.Model):
+class Pessoa(models.Model):
+    nome = models.CharField('Nome', max_length=100)
+
+    class Meta:
+        abstract = True
+        verbose_name = 'Professor'
+        verbose_name_plural = 'Professores'
+
+    def __str__(self):
+        return self.nome
+
+
+class Professor(Pessoa):
     OPCOES = (
         ('Doutorado',       'Doutorado'),
         ('Mestrado',        'Mestrado'),
         ('Especialização',  'Especialização'),
         ('Graduação',       'Graduação'),
     )
-    nome = models.CharField('Nome', max_length=100)
     titulacao = models.CharField('Titulação', blank=True, max_length=100, choices=OPCOES)
-    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    curso = models.ForeignKey(Curso, null=True, on_delete=models.SET_NULL)
 
     class Meta:
         verbose_name = 'Professor'
         verbose_name_plural = 'Professores'
 
+
+class Aluno(Pessoa):
+    matricula = models.IntegerField('Matrícula', unique=True)
+    data_nascimento = models.DateField('Data de Nascimento', blank=True, null=True, help_text='Formato DD/MM/AAAA')
+    email = models.EmailField('E-mail', blank=True, max_length=200)
+    curso = models.ForeignKey(Curso, on_delete=models.DO_NOTHING)
+
+    class Meta:
+        verbose_name = 'Aluno'
+        verbose_name_plural = 'Alunos'
+
+
+class Disciplina(models.Model):
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    nome = models.CharField('Nome', max_length=100)
+    carga_horaria = models.IntegerField('Carga horária')
+    obrigatoria = models.BooleanField('Obrigatória', default=True)
+    ementa = models.TextField('Ementa', blank=True, max_length=500)
+    bibliografia = models.TextField('Bibliografia', blank=True, max_length=500)
+
+    class Meta:
+        verbose_name = 'Disciplina'
+        verbose_name_plural = 'Disciplinas'
+
     def __str__(self):
         return self.nome
+
+
+class Turma(models.Model):
+    ano = models.IntegerField('Ano')
+    semestre = models.IntegerField('Semestre')
+    turma = models.CharField('Turma', max_length=10)
+    disciplina = models.ForeignKey(Disciplina, on_delete=models.CASCADE)
+    professor = models.ForeignKey(Professor, null=True, on_delete=models.SET_NULL)
+    alunos = models.ManyToManyField(Aluno)
+
+    class Meta:
+        verbose_name = 'Turma'
+        verbose_name_plural = 'Turmas'
+
+    def __str__(self):
+        return f"{self.ano} / {self.semestre} / {self.turma} / {self.disciplina}"
+
+
+class Nova(models.Model):
+    pass
