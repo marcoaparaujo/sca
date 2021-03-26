8a3efa5d5f8432b9e239d5a46afa0b73dd44c9d0
Marco Araujo <marcoaparaujo@gmail.com>
2021-03-26 15:44:10 -0300

    links

--- aplic/urls.py
+++ aplic/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import IndexView
+from .views import IndexView, SobreView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('sobre/', SobreView.as_view(), name='sobre'),
 ]
--- aplic/views.py
+++ aplic/views.py
@@ -3,3 +3,7 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+
+class SobreView(TemplateView):
+    template_name = 'about-us.html'
